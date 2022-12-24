6bb1f905e87b166231b4c2bb2ff6c3c8b8a11742
Sudono Tanjung <sudono.tanjung@klikdaily.com>
2022-12-24 16:17:32 +0700

    add documentation for change password

--- src/users/password.controller.ts
+++ src/users/password.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Put, Req, UseGuards } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Put, Req, UseGuards } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { SessionAuthGuard } from "./guards/session-auth.guard";
 import { JWTAuthGuard } from "./guards/jwt-auth.guard";
@@ -7,6 +7,7 @@ import { Roles } from "./roles.decorator";
 import { UserRole, UsersEntity } from "./users.entity";
 import { Request } from "express";
 import { ResponseFormatUtil } from "../utils/ResponseFormatUtil";
+import { ApiResponse } from "@nestjs/swagger";
 
 @Controller("/password")
 export class PasswordController {
@@ -14,6 +15,18 @@ export class PasswordController {
  }
 
  @Put("/")
+ @HttpCode(200)
+ @ApiResponse({
+  status: HttpStatus.OK,
+  description: 'Success change password.',
+  type: UsersEntity,
+  headers: {
+   Authorization: {
+    description: "token user will be store here at the header authorization response",
+    example: "Bearer eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0X3VzZXIiLCJpYXQiOjE2NzA3MzI1NTEsImV4cCI6MTY3MDgxODk1MX0.iw72t9fNF1_sAo_8Vpp7beGJZA46B7JD23Ey598jVni4jhtIjF0WD_akGjhVfgG5"
+   }
+  }
+ })
  @UseGuards(SessionAuthGuard, JWTAuthGuard, RolesGuard)
  @Roles(UserRole.ADMIN, UserRole.USER)
  async updatePassword(
